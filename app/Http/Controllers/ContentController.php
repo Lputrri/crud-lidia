51c0c6fc2b4fcc8ece9c29b809cf3f11458f6d7d
lputrri <putrikaligis15@gmail.com>
2023-10-26 10:14:02 +0700

    first commit

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Content;
+
+
+class ContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         $contents = content::all();
+        return view('contents.index', compact('contents'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+         return view('contents.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Content::create($request->all());
+
+        return redirect()->route('contents.index')->with('success', 'Konten berhasil ditambahkan.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $content = Content::findOrFail($id);
+        return view('contents.show', compact('content'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $content = Content::findOrFail($id);
+        return view('contents.show', compact('content'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $content = Content::findOrFail($id);
+        return view('contents.edit', compact('content'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $content = Content::findOrFail($id);
+        $content->delete();
+
+        return redirect()->route('contents.index')->with('success', 'Kontenberhasil dihapus.');
+    }
+}
